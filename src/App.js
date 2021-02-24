00b09f980402b792a919b23b9e399514bf604c77
Saeedullah15 <saeedullahazim65@gmail.com>
2021-02-25 00:20:21 +0600

    second commit

--- src/App.js
+++ src/App.js
@@ -5,21 +5,41 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 style = {{color: "red", backgroundColor: "white", width: "100%"}}>i am a react person</h1>
+        {/* component declaration */}
+        <Test></Test>
+        <Test></Test>
+        <Test></Test>
+        <Test></Test>
+        <Test></Test>
       </header>
     </div>
   );
+}
+
+// ****dynamic content and style, component creation, return multiple html from a component****
+const myObject = {
+  name : "saeedullah azim",
+  age : "22",
+  versity : "AIUB"
+}
+// const myStyle = {
+//   border: "4px solid grey",
+//   margin: "5px",
+//   borderRadius: "10px",
+//   padding: "5px",
+//   color: "orange"
+// }
+
+// component creation(first letter of component name must be in capital letter)
+function Test() {  
+  return (
+    <div style = {{border: "4px solid grey", margin: "5px", borderRadius: "10px", padding: "5px", color: "orange"}}>
+      <h1>name: {myObject.name}</h1>
+      <h3>age: {myObject.age}</h3>
+      <h3>versity: {myObject.versity}</h3>
+    </div>
+  )
 }
 
 export default App;
